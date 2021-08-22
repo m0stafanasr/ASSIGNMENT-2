3a18a5408a66cdc6cd9bf4992384276ce49186ef
mostafa nasr <74932029+m0stafanasr@users.noreply.github.com>
2021-08-22 08:52:30 +0200

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -50,47 +50,37 @@ var counter = 0;
         
         function birthDay(month) {
           
-            var day = prompt("please enter your day of birth:");
-            if (month == 4 || month == 6 || month == 9 || month == 11) {
-                if (day > 30 || day < 1 || isNaN(day)) {
-                    if (confirm('please enter a valid day')) {
+              var day = prompt("please enter your day of birth:");
+            if (month == 4 || month == 6 || month == 9 || month == 11 && day > 30 || day < 1 || isNaN(day)) {
+                   if (confirm('please enter a valid day')) {
                         birthDay();
                     }
                     else {
                         showName();
                     }
                 }
-                else {
-                    horscope(month, day);
-                }
-            }
-            else if (month == 2) {
-                    if (day > 29 || day < 1 || isNaN(day)) {
-                        if (confirm('please enter a valid day')) {
+            
+            else if (month == 2 && day > 29 || day < 1 || isNaN(day)) {
+                    if (confirm('please enter a valid day')) {
                             birthDay();
                         }
                         else {
                             showName();
                         }
                     }
-                    else {
-                        horscope(month, day);
-                    }
-                } 
-            else if ((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)) {
-                if (day > 31 || day < 1 || isNaN(day)) {
-                        if (confirm('please enter a valid day')) {
+               
+               
+            else if ((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) && day > 31 || day < 1 || isNaN(day)) {
+                if (confirm('please enter a valid day')) {
                             birthDay();
                         }
                         else {
                             showName();
                         }
                  }
-                 else {
+	  else {
                     horscope(month, day);
                 }
-                }
-            
         }
         function horscope(m, d) {
             // test 
